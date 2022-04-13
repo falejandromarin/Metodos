c7413d0d7e9002370420594168f3cd1652366b1e
Aprendiz Desarrollo <apdesarrollo@fenalcovalle.local>
2022-04-13 15:46:31 -0500

    Cambio de prueba

--- src/test/java/com/example/PrincipalTest.java
+++ src/test/java/com/example/PrincipalTest.java
@@ -12,19 +12,10 @@ class PrincipalTest {
 	System.out.println("suma");
 	Principal prin = new Principal(3,3,0);
     prin.suma();
-    int expResult = 6;
+    int expResult = 7;
     int result = prin.suma();
     assertEquals(expResult, result);
 }
 	
-	@Test
-	public void testmultiplicacion() {
 	
-	System.out.println("La multiplicacion es ");
-	Principal prin = new Principal(5,5,0);
-    prin.suma();
-    int expResult = 25;
-    int result = prin.suma();
-    assertEquals(expResult, result);
-}
 }
