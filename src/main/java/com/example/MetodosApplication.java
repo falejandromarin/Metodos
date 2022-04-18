678bd07f38e53fc95a76191778d126e51f0df333
Aprendiz Desarrollo <apdesarrollo@fenalcovalle.local>
2022-04-18 16:54:56 -0500

    18/04/2022

--- src/main/java/com/example/MetodosApplication.java
+++ src/main/java/com/example/MetodosApplication.java
@@ -2,10 +2,18 @@ package com.example;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+import com.example.MetodosApplication;
+
+import org.springframework.boot.SpringApplication;
+
+
 @SpringBootApplication
+
 public class MetodosApplication {
 
 	public static void main(String[] args) {
+		SpringApplication.run(MetodosApplication.class, args);
 		
 		Principal prin= new Principal(1,3,0,0);
 		
